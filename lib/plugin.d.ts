03df1f4fde87af13311f0986af98660147e7e8e9
Byron Varberg <byronvarberg@gmail.com>
2022-09-24 19:14:12 -0500

    First pass at building with vite

---
+++ lib/plugin.d.ts
@@ -0,0 +1,8 @@
+declare module "testdouble" {
+  /**
+   * Create a fake function component.
+   *
+   * @param displayName Name of function component for better messages.
+   */
+  export function component<F>(displayName?: string): F;
+}
---
+++ lib/testdouble-component.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react";
+
+function setup(testdouble: any) {
+  const td = testdouble;
+
+  function component<F>(displayName = "TestDoubleFunctionComponent") {
+    const ComponentDouble = td.function(displayName) as FunctionComponent;
+    ComponentDouble.displayName = displayName;
+
+    // see: https://github.com/testdouble/testdouble.js/issues/390
+    const anyProps = td.matchers.anything();
+    td.when(ComponentDouble(anyProps), { ignoreExtraArgs: true }).thenReturn(
+      <>{displayName}</>
+    );
+
+    return ComponentDouble as unknown as F;
+  }
+
+  td.component = component;
+}
+
+export default setup;
