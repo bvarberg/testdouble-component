a13bd24d65c2b86d65f6e5057e61957d99a13aa2
Byron Varberg <byronvarberg@gmail.com>
2022-10-10 21:07:22 -0500

    Update dependency specification
    And remove unused Vite setup for now.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from 'vite-plugin-dts'
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: "testdouble-component",
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react"],
       output: {
         globals: {
           react: "React",
